73bf0d89bf16b541c47d9f08c53097e7a371908c
NaseerAhmedWighio <NaseerAhmedWighio>
2024-10-21 14:02:30 +0500

    some errors

--- src/app/Components/header.tsx
+++ src/app/Components/header.tsx
@@ -27,7 +27,7 @@ export default function Header() {
                     </div>
 
                     {/* Navigation Links (visible on large screens) */}
-                    <div className="hidden lg:flex gap-12 text-right">
+                    <div className="hidden lg:flex gap-12 text-right text-white">
                         <Link href="/"><h1 className="text-2xl lg:text-xl text-orange-600 hover:text-white">HOME</h1></Link>
                         <h1 className="text-2xl lg:text-xl hover:text-orange-600 cursor-pointer">SERVICES</h1>
                         <h1 className="text-2xl lg:text-xl hover:text-orange-600 cursor-pointer">ABOUT ME</h1>
@@ -38,12 +38,12 @@ export default function Header() {
                     {/* Buttons (visible on large screens) */}
                     <div className="hidden lg:flex gap-4">
                         <Link href="/Signup">
-                            <button type="button" className="text-center text-xl font-bold py-3 px-6 hover:bg-white hover:text-orange-600 bg-orange-600 rounded-lg">
+                            <button type="button" className="text-center text-xl text-white font-bold py-3 px-6 hover:bg-white hover:text-orange-600 bg-orange-600 rounded-lg">
                                 Sign Up
                             </button>
                         </Link>
                         <Link href="/Login">
-                            <button type="button" className="text-center text-xl font-bold py-[7px] px-6 hover:bg-orange-600 hover:text-white border-2 border-orange-600 rounded-lg">
+                            <button type="button" className="text-center text-xl text-white font-bold py-[7px] px-6 hover:bg-orange-600 hover:text-white border-2 border-orange-600 rounded-lg">
                                 Log In
                             </button>
                         </Link>
@@ -52,7 +52,7 @@ export default function Header() {
 
                 {/* Mobile Menu (visible when menuOpen is true) */}
                 {menuOpen && (
-                    <div className="absolute right-0 top-full w-32 lg:hidden flex flex-col items-right mr-7 gap-3 text-right py-2">
+                    <div className="absolute right-0 top-full text-white w-32 lg:hidden flex flex-col items-right mr-7 gap-3 text-right py-2">
                         <Link href="/"><h1 className="text-xl text-orange-600 hover:text-white">Home</h1></Link>
                         <h1 className="text-lg hover:text-orange-600 cursor-pointer">SERVICES</h1>
                         <h1 className="text-lg hover:text-orange-600 cursor-pointer">ABOUT ME</h1>
@@ -60,12 +60,12 @@ export default function Header() {
                         <h1 className="text-lg hover:text-orange-600 cursor-pointer">CONTACT</h1>
 
                         <Link href="/Signup">
-                            <button type="button" className="text-center text-xl font-bold py-3 px-6 hover:bg-white hover:text-orange-600 bg-orange-600 rounded-lg">
+                            <button type="button" className="text-center text-xl font-bold py-3 px-6 text-white hover:bg-white hover:text-orange-600 bg-orange-600 rounded-lg">
                                 Sign Up
                             </button>
                         </Link>
                         <Link href="/Login">
-                            <button type="button" className="text-center text-xl font-bold py-2 px-[22px] hover:bg-orange-600 hover:text-white border-2 border-orange-600 rounded-lg">
+                            <button type="button" className="text-center text-xl font-bold py-2 text-white px-[22px] hover:bg-orange-600 hover:text-white border-2 border-orange-600 rounded-lg">
                                 Log In
                             </button>
                         </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Hero() {
       <div className="flex flex-col lg:flex-row items-center justify-between px-6 lg:px-20">
         {/* Text Section */}
         <div id="info-text" className="flex flex-col items-center mb-10 lg:mb-0 lg:items-start">
-          <div className="text-2xl sm:text-lg md:text-lg xl:text-3xl mb-2 text-center"> <TypeAnimation
+          <div className="text-2xl sm:text-lg md:text-lg xl:text-3xl mb-2 text-center text-white"> <TypeAnimation
             sequence={["Hi I'm", 1000, "Hi I am", 1000]}
             wrapper="span"
             speed={50}
@@ -27,7 +27,7 @@ export default function Hero() {
             repeat={Infinity}
           />
           </div>
-          <div className="text-3xl sm:text-2xl md:text-2xl xl:text-4xl font-bold mb-2 text-center"> <TypeAnimation
+          <div className="text-3xl sm:text-2xl md:text-2xl xl:text-4xl font-bold mb-2 text-center text-white"> <TypeAnimation
             sequence={["Naseer Ahmed", 1000, "Naseer Ahmed Wighio", 1000]}
             wrapper="span"
             speed={50}
@@ -49,25 +49,25 @@ export default function Hero() {
             <Link href="https://www.linkedin.com/in/naseer-ahmed-wighio-a20453285/">
               <TiSocialLinkedinCircular
                 title="LinkedIn"
-                className="w-8 h-8 sm:w-12 sm:h-12 hover:text-orange-600"
+                className="w-8 h-8 sm:w-12 sm:h-12 fill-white hover:text-orange-600"
               />
             </Link>
             <Link href="https://discord.com/">
               <AiOutlineDiscord
                 title="Discord"
-                className="w-8 h-8 sm:w-12 sm:h-12 hover:text-orange-600"
+                className="w-8 h-8 sm:w-12 sm:h-12 fill-white hover:text-orange-600"
               />
             </Link>
             <Link href="https://www.facebook.com/naseer.ahmed.3993">
               <PiFacebookLogoBold
                 title="Facebook"
-                className="w-8 h-8 sm:w-11 sm:h-11 hover:text-orange-600"
+                className="w-8 h-8 sm:w-11 sm:h-11 fill-white hover:text-orange-600"
               />
             </Link>
             <Link href="https://www.instagram.com/naseerahmedwighio/">
               <FaInstagram
                 title="Instagram"
-                className="w-8 h-8 sm:w-10 sm:h-10 hover:text-orange-600"
+                className="w-8 h-8 sm:w-10 sm:h-10 fill-white hover:text-orange-600"
               />
             </Link>
           </div>
@@ -76,13 +76,13 @@ export default function Hero() {
           <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 mb-6">
             <button
               type="button"
-              className="text-xl font-bold py-3 px-6 sm:px-14 hover:bg-white hover:text-orange-600 bg-orange-600 rounded-lg"
+              className="text-xl font-bold py-3 px-6 sm:px-14 text-white hover:bg-white hover:text-orange-600 bg-orange-600 rounded-lg"
             >
               Hire me
             </button>
             <button
               type="button"
-              className="text-xl font-bold py-3 px-6 sm:px-8 hover:bg-orange-600 hover:text-white border-2 border-orange-600 rounded-lg"
+              className="text-xl font-bold py-3 px-6 sm:px-8 text-white hover:bg-orange-600 hover:text-white border-2 border-orange-600 rounded-lg"
             >
               Download CV
             </button>
@@ -93,17 +93,17 @@ export default function Hero() {
             <div className="flex justify-between items-center space-x-5 ml-6 mr-6">
               <div className="text-center">
                 <h1 className="text-yellow-500 text-3xl font-semibold">5+</h1>
-                <p className="text-xl">Experiences</p>
+                <p className="text-xl text-white">Experiences</p>
               </div>
               <div className="w-[0.5px] h-36 opacity-70 bg-white"></div>
               <div className="text-center">
                 <h1 className="text-yellow-500 text-3xl font-semibold">20+</h1>
-                <p className="text-xl">Projects done</p>
+                <p className="text-xl text-white">Projects done</p>
               </div>
               <div className="w-[0.5px] h-36 opacity-70 bg-white"></div>
               <div className="text-center">
                 <h1 className="text-yellow-500 text-3xl font-semibold">80+</h1>
-                <p className="text-xl">Happy Clients</p>
+                <p className="text-xl text-white">Happy Clients</p>
               </div>
             </div>
           </div>
